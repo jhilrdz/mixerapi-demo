8d46ff431529d249cc29be6f835741af6d7a1f41
jhilrdz <jhilrdz@hotmail.com>
2022-07-06 17:09:35 -0500

    Config webapp/admin

--- app/plugins/Webapps/templates/Spa/view_homepage.php
+++ app/plugins/Webapps/templates/Spa/view_homepage.php
@@ -6,13 +6,13 @@ $pluginName = 'Webapps';
 if (Plugin::isLoaded($pluginName)) {
     $path = Plugin::path($pluginName) . 'webroot';
     if (is_dir($path)) {
-        $file = $path . DS . 'index.html';
+        $file = $path . DS . 'apps/admin/index.html';
         echo file_get_contents($file);
         return;
     }
 }
 
-echo '<h1>run npm build:prod</h1>'
+echo '<h2>Execute command In app/plugins/Webapps/frontend:  </h2><br/><h1>npm run build:prod</h1>';
 
 
 ?>
